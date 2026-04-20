f66325e808a6028f79d4ae3ba6453916f6fdce72
Neville Galletly <207318973+NGalletly@users.noreply.github.com>
2026-04-20 12:16:36 +0100

    translating typescript to python

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,27 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+app = Flask(__name__)
+
+CORS(app)
+
+@app.route('/calculate', methods =['POST'])
+
+def calculate():
+    data = request.json
+    init_amount = data.get('initAmount', 0)
+    contribution = data.get('annualContribution', 0)
+    expected_return = data.get('expectedReturn', 0)
+    duration = data.get('duration', 0)
+    final_balance = init_amount
+    total_deposits = init_amount
+
+    for _ in range(int(duration)):
+        final_balance+=contribution
+        total_deposits+=contribution
+        final_balance *= (1 + expected_return)
+    
+    return jsonify({
+        'finalBalance': round(final_balance,2),
+        'totalInterest': round(final_balance - total_deposits,2)
+    })
